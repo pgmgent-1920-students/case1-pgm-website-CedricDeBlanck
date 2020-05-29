e4d0705578fac6657a51af963d2bed1c3a33cbc5
CedricDeBlanck <cedridebl1@student.arteveldehs.be>
2020-05-29 17:11:27 +0200

    wip

--- docs/static/js/students.js
+++ docs/static/js/students.js
@@ -9,7 +9,7 @@ const fetchAPI = async (url) => {
 }
 
 async function dataToDom() {
-  const apidata = await fetchAPI('https://pgmgent-1920-students.github.io/case1-pgm-website-baas-CedricDeBlank/data/students/index.json')
+  const apidata = await fetchAPI('https://raw.githubusercontent.com/pgmgent-1920-students/case1-pgm-website-baas-CedricDeBlanck/master/docs/data/students/index.json?token=ANGVHBRF6XN26FEZIQYBOES63JQJA')
   apidata.records.forEach((i, index) => {
     const div = document.createElement('div');
     div.classList.add('a-student__content');
